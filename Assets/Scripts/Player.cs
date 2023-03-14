a0d25c69b473b417e319cde025a6cd2c96539120
SmokinVita <69760636+SmokinVita@users.noreply.github.com>
2023-03-15 05:18:13 +0900

    Finished Character Controller Section.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,12 @@ public class Player : MonoBehaviour
     private float _speed = 5f;
     [SerializeField]
     private float _gravity = 9.81f;
+    [SerializeField]
+    private float _jumpHeight = 15f;
+
+    private float _yVelocity;
+
+    private bool _canDoubleJump;
 
     void Start()
     {
@@ -23,16 +29,28 @@ public class Player : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        Vector3 direction = new Vector3(horizontalInput, 0);
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
         Vector3 velocity = direction * _speed;
 
-        if(_characterController.isGrounded == true)
+        if (_characterController.isGrounded == true)
         {
-            //do nothing atm
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
         }else
         {
-            velocity.y -= _gravity;
+            if (Input.GetKeyDown(KeyCode.Space) && _canDoubleJump == true)
+            {
+                _yVelocity += _jumpHeight;
+                _canDoubleJump = false;
+            }
+
+            _yVelocity -= _gravity;
         }
+
+        velocity.y = _yVelocity;
 
         _characterController.Move(velocity * Time.deltaTime);
     }
